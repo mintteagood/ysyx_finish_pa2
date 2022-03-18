3159cb3b77f0a1171b82ef7b89e2ae462eacfe58
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-18 10:02:14 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-36-generic #41~20.04.1-Ubuntu SMP Tue Mar 8 09:16:51 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:02:14 up 46 min,  1 user,  load average: 0.53, 0.20, 0.09

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -118,7 +118,7 @@ static int cmd_x(char *args) {
   /* extract the first argument */
   char *arg = strtok(NULL, " ");
   char *arg1 = strtok(NULL, " ");
-  char *arg2 = strtok(arg1, "x");
+  char *arg2 = strtok(arg1, "0x");
   int arg_l=atoi(arg);
   
   printf("return'%d'\n", arg_l);
