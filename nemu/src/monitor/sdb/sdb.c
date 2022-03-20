53124bb983bcf0d0dee5e80ecdf197125f345554
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 11:35:08 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-36-generic #41~20.04.1-Ubuntu SMP Tue Mar 8 09:16:51 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:35:08 up  2:51,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -89,12 +89,12 @@ static int cmd_help(char *args) {
 static int cmd_si(char *args) {
   /* extract the first argument */
   char *arg = strtok(NULL, " ");
-  int n = atoi(arg);
+  int n1 = atoi(arg);
   if (arg == NULL) {
     cpu_exec(1);
   }
   else {
-    cpu_exec(n);
+    cpu_exec(n1);
     }
   return 0;
 }
