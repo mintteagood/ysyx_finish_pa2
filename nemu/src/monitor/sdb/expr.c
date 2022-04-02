2cf289efe6748172e791305551aa6c3b939747fe
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-02 21:36:58 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:36:58 up  2:36,  1 user,  load average: 0.08, 0.06, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -211,7 +211,7 @@ word_t expr(char *e, bool *success) {
    Token *p = NULL;
    p = tokens;
    Token *q = NULL;
-   q = tokens+5;
+   q = tokens+4;
    return  eval(p,q);
  /*  return printf("%d",position);*/
    
