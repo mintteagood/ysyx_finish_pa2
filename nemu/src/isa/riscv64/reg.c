c3734a4d3a35c59c0165bea20788721e9a08ea62
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 21:02:36 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:02:36 up  7:41,  1 user,  load average: 0.17, 0.09, 0.02

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -19,7 +19,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   for (int i=0;i<32;i++){
   	if(s!=regs[i]){
   	/*return cpu.gpr[i];*/
-  	return printf("jicunqi%s and %s",regs[i],s);
+  	return printf("jicunqi%s and %s",s,regs[i]);
   	
   	*success = true;
   	return 0;
