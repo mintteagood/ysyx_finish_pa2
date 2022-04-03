e3b6b065691b093ba2b4feec89c2dd3fc32005a5
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 17:04:51 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:04:51 up  4:24,  1 user,  load average: 0.21, 0.09, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -181,7 +181,7 @@ int eval(Token *p,Token *q){
 	if(p->type== TK_REG){
 	 char sh[128];
 		sprintf(sh,"%d",p->type);
-		
+		printf("jixu");
 		return isa_reg_str2val(sh, success);
 			}
     if (p == q) 
