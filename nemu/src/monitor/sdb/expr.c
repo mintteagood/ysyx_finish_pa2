868e9f4b3d895aba441b5134324f7609da683479
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 22:56:15 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:56:15 up  9:06,  1 user,  load average: 0.00, 0.03, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -31,7 +31,7 @@ static struct rule {
   {"([1-9][0-9]{1,31})|[0-9]",TK_NUM},
   {"==", TK_EQ},        // equal
   {"0[xX][0-9a-fA-F]+",TK_HEX},
-  {"(\\$[0a-zA-Z]+)|([a-zA-Z][0-9]+)|([a-zA-Z][a-zA-Z][0-9]+)",TK_REG},
+  {"(\\$[0a-zA-Z]+)|([a-zA-Z][0-9]+)|([a-zA-Z][a-zA-Z]+)",TK_REG},
   
 };
 
