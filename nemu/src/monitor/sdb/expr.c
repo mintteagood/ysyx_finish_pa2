2d886911d2f0f785f0a102298107bc5786c08785
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 16:23:51 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:23:51 up  3:43,  1 user,  load average: 0.21, 0.07, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -196,7 +196,7 @@ int eval(Token *p,Token *q){
 			case '*': return val1 * val2;
 			case '/': return val1 / val2;
 			/*case TK_REG: return isa_reg_str2val((tokens+cal-1)->str, success);*/
-			case TK_REG: return isa_reg_str2val((tokens+cal-1)->str, success);
+			case TK_REG: return 12;
     	}
 	}
 	return 0;
