71cb958fab9c4ef4ede7f15cf32fc67cffb576cd
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-06 11:21:12 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:21:12 up 48 min,  1 user,  load average: 0.32, 0.11, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -179,6 +179,7 @@ Token* pos_mop(Token *p,Token *q){
 		}
 		else if (sign<=0 && (iter_p->type=='*' || iter_p->type=='/')){
 			pos_mod=iter_p;
+			printf("/nzhuyunsuanfu %d",pos_mod->type);
 			}
 	}
 
