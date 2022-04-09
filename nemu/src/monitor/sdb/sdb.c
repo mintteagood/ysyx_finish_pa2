57d2a303fc9174a13e9efffa811c63ed7c39159d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-10 02:46:02 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:46:02 up 12:49,  1 user,  load average: 0.02, 0.09, 0.05

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -137,7 +137,11 @@ return 0 ;
 }
 
 static int cmd_w(char *args){
-print_wp();
+WP* str=new_wp(args);
+/*char new_value = expr(str->exp,p);
+char* new_value2=&new_value;
+int new = atoi(new_value2);  */
+printf("监视点序号%d   表达式%s 值0x%08x\n",str->NO , str->exp, str->value);
 return 0 ;
 }
 
