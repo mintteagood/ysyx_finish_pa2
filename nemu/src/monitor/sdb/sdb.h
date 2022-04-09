6b0eebc99f6ed6fd1d56d3616f5090bd389ece72
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-09 09:43:44 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:43:44 up 12 min,  1 user,  load average: 0.23, 0.27, 0.19

--- nemu/src/monitor/sdb/sdb.h
+++ nemu/src/monitor/sdb/sdb.h
@@ -5,15 +5,5 @@
 
 word_t expr(char *e, bool *success);
 
-typedef struct watchpoint {
-  int NO;
-  struct watchpoint *next;
-  unsigned int value;
-  char* expr;
-  bool isused;
-  /* TODO: Add more members if necessary */
-
-} WP;
-WP *new_wp(char *str , int value);
 
 #endif
--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -2,53 +2,12 @@
 
 #define NR_WP 32
 
-/*typedef struct watchpoint {
+typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
-  unsigned int value;
-  char* expr;
-  bool isused;
-   TODO: Add more members if necessary 
 
-} WP;*/
-bool su =true;
-/*static WP wp_pool[NR_WP] = {};*/
-static WP *head = NULL, *free_ = NULL;
-void init_wp_pool(){
- /* head->NO = 0;
-  head->next = NULL;
-  head->value = 0;
-  head->expr = NULL;
-  head->isused = false;*/
-  
-/*  free_ ->NO = 1;
-  free_ ->next = NULL;
-  free_ ->value = 0;
-  free_ ->expr = NULL;
-  free_ ->isused = false;*/
-  }
 
-WP *new_wp(char *str , int value){
-   if(su == true){
-       init_wp_pool();
-       su = false;
-   }
-   if(free_ == NULL){
-       printf("Erro!free is null.\n");
-       assert(0);
-   }
-   WP *new = NULL;
-   new = free_;
-   free_ = free_->next;
-   new->value = value;
-   strcpy(new->expr, str);
-   new->next = NULL;
-   new->isused = true;
-   printf("add a watchpoint %s 0x%08x\n",new->expr,new->value);
-   if(head == NULL) head = new;
-   else{
-       new->next = head;
-       head = new ;
-   }
-   return new;
-} 
+} WP;
+
+
+
