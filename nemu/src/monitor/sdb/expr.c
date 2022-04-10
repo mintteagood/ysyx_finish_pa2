c07de1ce7d9587b3a5a412e69c8f88ce34504787
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-10 20:35:21 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:35:21 up  1:32,  1 user,  load average: 0.23, 0.18, 0.11

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -105,7 +105,6 @@ static bool make_token(char *e) {
 		tokens[nr_token].type = rules[i].token_type;
 		strncpy(tokens[nr_token].str,substr_start,substr_len);
 		tokens[nr_token++].str[substr_len] = '\0';
-		printf("ok");
 		break;
 		}
 		case TK_NOTYPE:
