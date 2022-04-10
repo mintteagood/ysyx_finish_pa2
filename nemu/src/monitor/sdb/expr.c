73af049a62a0e429e0647100e97a65eea3fac4c2
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-10 20:31:33 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:31:33 up  1:28,  1 user,  load average: 0.17, 0.12, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -8,128 +8,100 @@
 
 #include <memory/paddr.h>
 
-enum {
-  TK_NOTYPE = 256, TK_EQ,TK_NUM,TK_HEX,TK_REG,DEREF,
-  /* TODO: Add more token types */
+#include <sys/types.h>
+#include <regex.h>
+#include <stdlib.h>
 
+enum {
+    TK_NOTYPE = 256, TK_NUM,  
 };
 
 static struct rule {
-  const char *regex;
-  int token_type;
+    char *regex;
+    int token_type;
 } rules[] = {
-
-  /* TODO: Add more rules.
-   * Pay attention to the precedence level of different rules.
-   */
-
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"-",'-'},            //decrease
-  {"\\*",'*'},          //multiply 
-  {"/",'/'},            //divide
-  {"\\(",'('} ,         //left parenthesis    
-  {"\\)",')'} ,         //right parenthesis 
-  {"(0x[0-9a-fA-F]+)",TK_HEX},
-  {"([1-9][0-9]{1,31})|[0-9]",TK_NUM},
-  {"==", TK_EQ},        // equal
-  
-  {"(\\$[0a-zA-Z]+)|([a-zA-Z][0-9]+)|([a-zA-Z][a-zA-Z]+)",TK_REG},
-  
+    {" +", TK_NOTYPE},            			// spaces
+    {"\\+", '+'},                 			// plus
+	{"-", '-'},								// decrease
+	{"\\*", '*'},							// multiply
+	{"/", '/'},								// divide
+	{"\\(",'('},							// Left parenthesis
+	{"\\)",')'},							// Right parenthesis
+	{"([1-9][0-9]{1,31})|[0-9]",TK_NUM},	// Decimal integer	
 };
 
-#define NR_REGEX ARRLEN(rules)
+#define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
 
-static regex_t re[NR_REGEX] = {};
+static regex_t re[NR_REGEX];
 
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
  */
 void init_regex() {
-  int i;
-  char error_msg[128];
-  int ret;
+    int i;
+    char error_msg[128];
+    int ret;
 
-  for (i = 0; i < NR_REGEX; i ++) {
-    ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-    if (ret != 0) {
-      regerror(ret, &re[i], error_msg, 128);
-      panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+    for (i = 0; i < NR_REGEX; i ++) {
+        ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+        if (ret != 0) 
+            regerror(ret, &re[i], error_msg, 128);
     }
-  }
 }
 
 typedef struct token {
-  int type;
-  char str[32];
+    int type;
+    char str[32];
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
-static int nr_token __attribute__((used))  = 0;
+Token tokens[10000];
+int nr_token;
 int cal;
-word_t isa_reg_str2val(const char *s, bool *success);
 static bool make_token(char *e) {
-  int position = 0;
-  int i;
-  regmatch_t pmatch;
+    int position = 0;
+    int i;
+    regmatch_t pmatch;
 
-  nr_token = 0;
+    nr_token = 0;
 
-  while (e[position] != '\0') {
-    /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-      
-        char *substr_start = e + position;
-        int substr_len = pmatch.rm_eo;
-		
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
-        position += substr_len;
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
-         * of tokens, some extra actions should be performed.
-         */
-	
-        switch (rules[i].token_type) {
-        	case '+':
-		case '-':
-		case '*':
-		case '/':
-		case '(':
-		case ')':
-		case TK_NUM:
-		case TK_REG:
-		case TK_HEX:
-	       {
-		tokens[nr_token].type = rules[i].token_type;
-		strncpy(tokens[nr_token].str,substr_start,substr_len);
-		tokens[nr_token++].str[substr_len] = '\0';
-		printf("ok");
-		break;
-		}
-		case TK_NOTYPE:
-			break;
-          	default: TODO();
+    while (e[position] != '\0') {
+        /* Try all rules one by one. */
+        for (i = 0; i < NR_REGEX; i ++) {
+            if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+                char *substr_start = e + position;
+                int substr_len = pmatch.rm_eo;
+                
+                position += substr_len;
+                switch (rules[i].token_type) {
+					case '+':
+					case '-':
+					case '*':
+					case '/':
+					case '(':
+					case ')':
+					case TK_NUM:
+					{
+						tokens[nr_token].type = rules[i].token_type;
+						strncpy(tokens[nr_token].str, substr_start, substr_len);
+						tokens[nr_token++].str[substr_len] = '\0';
+						break;
+					}
+					case TK_NOTYPE:
+						break;
+                }
+                break;
+            }
         }
-	break;
+
+        if (i == NR_REGEX) {
+            printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+            return false;
+        }
     }
-	
-    if (i == NR_REGEX) {
-      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-      return false;
-    }
-    }
-  }
-  printf("token_str=%s",tokens[nr_token-1].str);
-	cal=nr_token;
-  return true;
+    cal=nr_token;
+
+    return true;	
 }
-	
-
-
-
-
 
 bool check_parentheses(Token *p,Token *q){
 	int count = 0;
@@ -154,10 +126,8 @@ Token* pos_mop(Token *p,Token *q){
 	int is_inP=0;
     int sign=0; 
 	Token *pos_mod=NULL;
-	
 
 	for (Token *iter_p=p;iter_p<=q;iter_p++) {
-	
 		if (iter_p->type=='(') {
 			is_inP++;
 			continue;
@@ -175,35 +145,20 @@ Token* pos_mop(Token *p,Token *q){
 			pos_mod=iter_p;
 			sign=1;
 		}
-		else if (sign<=0 && (iter_p->type=='*' || iter_p->type=='/')){
+		else if (sign<=0 && (iter_p->type=='*' || iter_p->type=='/'))
 			pos_mod=iter_p;
-			printf("zhuyunsuanfu %d",pos_mod->type);
-			}
 	}
 
 	return pos_mod;
 }
-bool *success;
+
 int eval(Token *p,Token *q){
-	if(p->type== TK_REG){
-	 	char sh[128];
-		sprintf(sh,"%d",p->type);
-		printf(" zhixiang%s",p->str);
-		return isa_reg_str2val(p->str, success);
-		
-			}
-	else if(p->type== DEREF){
-		return printf("zhizhenzhi %ld",paddr_read((int)atoi(p->str), 4));
-			
-		}
-	else{
-      if (p == q) 
+    if (p == q) 
         return (int)atoi(p->str);	
   	else if (check_parentheses(p, q ) == true)
     	return eval(p + 1, q - 1);
   	else {
 		int val1=1,val2;
-		if(p->type != TK_REG){
 		Token *op = pos_mop(p, q);
 		val1 = eval(p, op - 1);
 		val2 = eval(op + 1, q);
@@ -212,18 +167,12 @@ int eval(Token *p,Token *q){
 			case '-': return val1 - val2;
 			case '*': return val1 * val2;
 			case '/': return val1 / val2;
-			
-			}
-			
-    		}
+    	}
 	}
 	return 0;
-	}
-	
 }
 
-
-
+bool *success;
 
 
 word_t expr(char *e, bool *success) {
@@ -232,11 +181,6 @@ word_t expr(char *e, bool *success) {
     return 0;
   }
    else{
-   for (int i = 0; i < nr_token; i ++) {
-  if (tokens[i].type == '*' && (i == 0 || tokens[i - 1].type == '*') ) {
-    tokens[i].type = DEREF;
-  }
-}
    Token *p = NULL;
    p = tokens;
    Token *q = NULL;
