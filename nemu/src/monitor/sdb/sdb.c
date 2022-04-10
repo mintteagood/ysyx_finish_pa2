8dc2fda0e85168fcad72003c28a38ce72032516e
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-11 00:38:48 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:38:48 up  5:36,  1 user,  load average: 0.08, 0.03, 0.03

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -116,8 +116,8 @@ static int cmd_info(char *args) {
   else {
     if (strcmp(arg,"r") == 0)
      isa_reg_display();
-    if (strcmp(arg,"w") == 0)
-     print_wp();
+   /* if (strcmp(arg,"w") == 0)
+     print_wp();*/
     }
   return 0;
 }
