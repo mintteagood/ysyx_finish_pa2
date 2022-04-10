4c17bc85755062754341672ce0e1a8f10ceb476d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-10 22:42:51 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:42:51 up  3:40,  1 user,  load average: 0.13, 0.05, 0.01

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -24,8 +24,9 @@ run: run-env
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
-	gdb -s $(BINARY) --args $(NEMU_EXEC) 
-    
+	gdb -s $(BINARY) --args $(NEMU_EXEC)
+	CFLAGS += -D_DEBIG -O0 -G -DEBUG=1 -W1	 
+	
 
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
 $(clean-tools):
