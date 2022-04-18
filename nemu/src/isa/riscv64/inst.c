ef456c7f490f8d8a8bb01c0c21f08849dfe5adc1
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-19 00:28:57 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:28:57 up 10:44,  1 user,  load average: 0.25, 0.13, 0.04

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -9,7 +9,7 @@
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, // none
+  TYPE_N,TYPE_J // none
 };
 
 #define src1R(n) do { *src1 = R(n); } while (0)
@@ -19,9 +19,11 @@ enum {
 #define src2I(i) do { *src2 = i; } while (0)
 #define destI(i) do { *dest = i; } while (0)
 
+
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
+static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 20) |(SEXT(BITS(i, 19, 12), 1) << 19)|(SEXT(BITS(i, 20, 20),1) << 11)| BITS(i,30,21); }
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
@@ -33,6 +35,7 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
     case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
+    case TYPE_J: src1I(immJ(i));  break;
   }
 }
 
@@ -54,6 +57,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(dest) = src1 + src2);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(dest) = 4 + s->pc , s->pc = s->pc+src1);
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
