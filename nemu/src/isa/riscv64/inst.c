5a2e9c469ba931da97af35dab8fc3bb24236605d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-20 23:15:54 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:15:54 up  6:03,  1 user,  load average: 0.04, 0.03, 0.00

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -24,7 +24,7 @@ static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 //static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
 static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 19) |BITS(i, 19, 12) << 12|BITS(i, 20, 20) << 11| BITS(i,30,21)<<1; }
-static word_t immB(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 12) |BITS(i, 7, 7) << 11|BITS(i, 30,25)<<5| BITS(i,11,8)<<1; }
+static word_t immB(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 11) |BITS(i, 7, 7) << 11|BITS(i, 30,25)<<5| BITS(i,11,8)<<1; }
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
@@ -61,7 +61,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add  ,   S, R(dest) = src1 + src2,printf("addok\n"));
   INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub  ,   S, R(dest) = src1 - src2,printf("subok\n"));
   INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu,   I, R(dest) = src1 < ((unsigned int) src2 << 1) ? 1:0,printf("sltiu is:%lx\n",R(dest)));
-  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq  ,   B, s->dnpc = (src1==src2) ? s->pc+R(dest) :  s->pc+R(dest) ,printf("beq next s->dnpc is:%lx\n",s->dnpc),printf("beq next s->snpc is:%lx\n",s->snpc),printf("beq R(DEST) is:%lx\n",R(dest)));
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq  ,   B, s->dnpc = (src1==src2) ? s->pc+R(dest) :  s->pc ,printf("beq next s->dnpc is:%lx\n",s->dnpc),printf("beq next s->snpc is:%lx\n",s->snpc),printf("beq R(DEST) is:%lx\n",R(dest)));
 
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + src2, 8));
   INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + dest, 8, src2));
