cbcdd14ed34e6913399adf7214f6aea31360d7e5
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 21:52:34 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:52:34 up  6:24,  1 user,  load average: 0.09, 0.13, 0.09

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -108,6 +108,8 @@ static int decode_exec(Decode *s) {
  
 //to-lower-case
   INSTPAT("??????? ????? ????? 110 ????? 11000 11", bltu  ,  B, s->dnpc = (src1< src2) ? s->pc + dest :  s->pc+4  ,printf("current pc is %lx ",s->pc),printf("blt next s->dnpc is:%lx\n",s->dnpc),printf("bltsrc1 is:%lx\n",src1),printf("bltsrc2 is:%lx\n",src2),printf("R(10) is:%lx\n",R(10)));
+//string
+  INSTPAT("??????? ????? ????? 111 ????? 11000 11", bgeu  ,  B, s->dnpc = (src1 >= src2) ? s->pc + dest :  s->pc+4  ,printf("current pc is %lx ",s->pc),printf("bgeu next s->dnpc is:%lx\n",s->dnpc),printf("bltsrc1 is:%lx\n",src1),printf("bltsrc2 is:%lx\n",src2),printf("R(10) is:%lx\n",R(10)));
 
 
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = src1 + s->pc,printf("current pc is %lx ",s->pc),printf("auipcok\n"),printf("R(10) is:%lx\n",R(10)));
