e2f14cd7bc9aa415d50a4d7db7fd92c5ea572414
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 19:25:28 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:25:28 up  3:57,  1 user,  load average: 0.04, 0.03, 0.00

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -84,7 +84,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 101 ????? 11000 11", bge  ,   B, s->dnpc = (SEXT(src1,5) >= SEXT(src2,5)) ? s->pc + dest :  s->pc+4  ,printf("current pc is %lx ",s->pc),printf("bge next s->dnpc is:%lx\n",s->dnpc),printf("bnesrc1 is:%lx\n",src1),printf("bnesrc2 is:%lx\n",src2),printf("R(10) is:%lx\n",R(10)));
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw    ,  S, Mw(src1 + dest, 4, BITS(src2,31,0)),printf("current pc is %lx ",s->pc),printf("R(10) is:%lx\n",R(10)));
 
-  INSTPAT("0000001 ????? ????? 000 ????? 01110 11", mulw  ,  RR, R(dest) = SEXT(BITS(src1 * src2,31,0),32),printf("current pc is %lx ",s->pc),printf("mulwok\n ,jieguo is:%lx\n",R(dest)),printf("R(10) is:%lx\n",R(10)));
+  //INSTPAT("0000001 ????? ????? 000 ????? 01110 11", mulw  ,  RR, R(dest) = SEXT(BITS(src1 * src2,31,0),32),printf("current pc is %lx ",s->pc),printf("mulwok\n ,jieguo is:%lx\n",R(dest)),printf("R(10) is:%lx\n",R(10)));
   INSTPAT("0000001 ????? ????? 100 ????? 01110 11", divw  ,  RR, R(dest) = SEXT((BITS(src1 ,31,0)/BITS(src2,31,0)),32),printf("current pc is %lx ",s->pc),printf("addok\n ,jieguo is:%lx\n",R(dest)),printf("R(10) is:%lx\n",R(10)));
 //if-else
   INSTPAT("0000001 ????? ????? 110 ????? 01110 11", remw  ,  RR, R(dest) = SEXT((BITS(src1 ,31,0)%BITS(src2,31,0)),32),printf("current pc is %lx ",s->pc),printf("addok\n ,jieguo is:%lx\n",R(dest)),printf("R(10) is:%lx\n",R(10)));
