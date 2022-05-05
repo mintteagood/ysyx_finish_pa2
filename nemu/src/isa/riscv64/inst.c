4ad0f5636b382d86b19a66d03aafc0af49e0c85f
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 13:12:18 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:12:18 up 21 min,  1 user,  load average: 0.16, 0.15, 0.14

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -80,7 +80,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb    ,  S, Mw(src1 + dest, 1, BITS(src2,7,0)),printf("current pc is %lx ",s->pc),printf("R(10) is:%lx\n",R(10)));
   INSTPAT("0000000 ????? ????? 100 ????? 01100 11", xor ,    RR,  R(dest) = src1 ^ src2 , printf("current pc is %lx ",s->pc),printf("R(10) is:%lx\n",R(10)));
 
-  INSTPAT("000000? ????? ????? 101 ????? 00100 11", srli ,   I, R(dest) = SEXT((unsigned int)BITS((src1 >> src2), 4, 0),5) ,printf("current pc is %lx ",s->pc),printf("R(10) is:%lx\n",R(10)));
+  INSTPAT("000000? ????? ????? 101 ????? 00100 11", srli ,   I, R(dest) = src1 >> src2 ,printf("current pc is %lx ",s->pc),printf("R(10) is:%lx\n",R(10)));
   INSTPAT("??????? ????? ????? 101 ????? 11000 11", bge  ,   B, s->dnpc = (src1>=src2) ? s->pc + dest :  s->pc+4  ,printf("current pc is %lx ",s->pc),printf("bge next s->dnpc is:%lx\n",s->dnpc),printf("bnesrc1 is:%lx\n",src1),printf("bnesrc2 is:%lx\n",src2),printf("R(10) is:%lx\n",R(10)));
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw    ,  S, Mw(src1 + dest, 4, BITS(src2,31,0)),printf("current pc is %lx ",s->pc),printf("R(10) is:%lx\n",R(10)));
 
