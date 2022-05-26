8d2728ad0db9ff49a68d1cfda25321e3696dea73
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-26 20:35:56 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:35:56 up  2:55,  0 users,  load average: 0.18, 0.08, 0.02

--- npc/csrc/paddr.c
+++ npc/csrc/paddr.c
@@ -9,6 +9,15 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
+static inline word_t host_read(void *addr, int len) {
+  switch (len) {
+    case 1: return *(uint8_t  *)addr;
+    case 2: return *(uint16_t *)addr;
+    case 4: return *(uint32_t *)addr;
+    IFDEF(CONFIG_ISA64, case 8: return *(uint64_t *)addr);
+    default: MUXDEF(CONFIG_RT_CHECK, assert(0), return 0);
+  }
+}
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
