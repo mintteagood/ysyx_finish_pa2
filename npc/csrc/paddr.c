3632290db1100c1b75ea2db05facafcfc9ac4ade
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-26 20:39:47 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:39:47 up  2:59,  0 users,  load average: 0.01, 0.04, 0.00

--- npc/csrc/paddr.c
+++ npc/csrc/paddr.c
@@ -50,8 +50,8 @@ word_t paddr_read(paddr_t addr, int len) {
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
 }
 
-void paddr_write(paddr_t addr, int len, word_t data) {
+/*void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
-}
+}*/
