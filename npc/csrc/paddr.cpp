1141114cca1ac9b92cd1a55da6579633234211cc
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 01:02:05 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:02:05 up  2:40,  1 user,  load average: 0.00, 0.03, 0.00

--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -30,7 +30,7 @@ word_t host_read(void *addr, int len) {
 word_t pmem_read(paddr_t addr, int len) {
    printf(" pmem_readok1\n");
    //printf(" guest_to_host okpmem%hhn\n", guest_to_host(addr));
-   word_t ret =host_read( guest_to_host(addr),1);
+   word_t ret =* guest_to_host(addr);
    printf("addr: %x",addr);
     printf("addr: %lx",ret);
   //word_t ret = 0x80000000;
