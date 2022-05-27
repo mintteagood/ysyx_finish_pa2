07f6de3b14028003bd5a4c50522159e5c0a74d0d
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 01:45:46 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:45:46 up  3:23,  1 user,  load average: 0.03, 0.04, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char **argv, char **env) {
   for (i=0; i<2; i++) {
     top->rst = (i < 0);
     // dump variables into VCD file and toggle clock
-    for (clk=0; clk<2; clk++) {
+    for (clk=0; clk<10; clk++) {
       tfp->dump (2*i+clk);
       top->clk = !top->clk;
       uint32_t pc = top->addr;
