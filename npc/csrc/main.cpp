e46c87008233a5bffaf3030f0943b6c5e909efcc
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 00:50:58 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:50:58 up  2:28,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv, char **env) {
   // init top verilog instance
   Vysyx_22040175_top* top = new Vysyx_22040175_top;
   // init trace dump
-  Verilated::traceEverOn(true);
+ // Verilated::traceEverOn(true);
   //VerilatedVcdC* tfp = new VerilatedVcdC;
   //top->trace (tfp, 99);
   //tfp->open ("Vysyx_22040175.vcd");
