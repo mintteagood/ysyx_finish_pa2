f1657e5539c97b888aaabae4ec3e0e5c388c4873
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 02:11:40 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:11:40 up  3:49,  1 user,  load average: 0.09, 0.07, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -34,11 +34,14 @@ int main(int argc, char **argv, char **env) {
       tfp->dump (2*i+clk);
       top->clk = !top->clk;
       uint32_t pc = top->addr;
-    if (Verilated::gotFinish())  exit(0);
+    
   }
-    top->instr = pmem_read(top->addr,3);
+      top->instr = pmem_read(top->addr,3);
       top->eval ();
-  //tfp->close();
+  }
+  if (Verilated::gotFinish())  exit(0);
+    
+  tfp->close();
   exit(0);
 }
 
