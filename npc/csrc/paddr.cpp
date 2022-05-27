294290bae935684944b1d9e8a369828dab180c65
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-27 23:44:04 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:44:04 up  1:22,  1 user,  load average: 0.16, 0.08, 0.02

--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -29,6 +29,7 @@ word_t host_read(void *addr, int len) {
   }
 }
 paddr_t pmem_read(paddr_t addr, int len) {
+   printf(" pmem_read ok1\n");
   paddr_t ret = host_read(guest_to_host(addr), len);
   return ret;
   printf(" pmem_read ok\n");
