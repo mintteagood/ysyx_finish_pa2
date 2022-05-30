c286eb8c5eda85c635dc371488adc9e87d409ee3
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 10:52:06 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:52:06 up  6:25,  1 user,  load average: 0.01, 0.06, 0.02

--- npc/csrc/dut.c
+++ npc/csrc/dut.c
@@ -3,7 +3,7 @@
 #include "../csrc/utils.h"
 #include "../csrc/difftest-def.h"
 #include <dlfcn.h>
-
+#include <stdio.h>
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
