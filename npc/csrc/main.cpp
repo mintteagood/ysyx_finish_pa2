cf41abba03f37cf47002e53e815ec46349fb12f2
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 10:20:11 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:20:11 up  5:53,  1 user,  load average: 0.04, 0.05, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -28,7 +28,6 @@ int main(int argc, char **argv, char **env) {
   char* img_file = *(argv + 1);
   init_imem();
   long img_size = load_img(img_file);
-// init_difftest(img_file,img_size,port);
   for (i=0; i<20; i++) {
     top->rst = (i < 2);
     // dump variables into VCD file and toggle clock
@@ -39,7 +38,6 @@ int main(int argc, char **argv, char **env) {
   }
      if(top->clk==1){
       top->inst = pmem_read(top->curr_pc,3);
-     /*difftest_step(top->curr_pc,top->next_pc);*/
      }
   }
   if (Verilated::gotFinish())  exit(0);
