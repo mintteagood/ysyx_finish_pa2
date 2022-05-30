a2b9195d15a72bacc0eabadcb083034818295784
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-30 10:15:23 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:15:23 up  5:49,  1 user,  load average: 0.00, 0.01, 0.00

--- npc/csrc/dut.c
+++ npc/csrc/dut.c
@@ -84,7 +84,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int i;
   for (i = 0; i < 32; i++) {
     if (ref_r->gpr[i] != gpr(i)) {
-      isa_reg_display(ref_r, pc);
+     // isa_reg_display(ref_r, pc);
       return false;
     }
   }
@@ -97,7 +97,7 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
-    isa_reg_display();
+    //isa_reg_display();
   }
 }
 
