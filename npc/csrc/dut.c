1beb8f39647d055c858b22bcc9b3c63142ee6c0c
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-02 12:06:25 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:06:25 up  1:04,  1 user,  load average: 0.00, 0.02, 0.01

--- npc/csrc/dut.c
+++ npc/csrc/dut.c
@@ -22,7 +22,7 @@ void difftest_raise_intr(word_t NO) {
 
 
 
-void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void* (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
@@ -69,7 +69,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   assert(handle);
 
   ref_difftest_memcpy = dlsym(handle, "difftest_memcpy");
-  assert(ref_difftest_memcpy);
+  assert(*ref_difftest_memcpy);
 
   ref_difftest_regcpy = dlsym(handle, "difftest_regcpy");
   assert(ref_difftest_regcpy);
