0d2ba52074ce8e068b539f1b0fa130b2735d8917
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-02 12:45:32 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:45:32 up  1:43,  1 user,  load average: 0.03, 0.05, 0.00

--- npc/csrc/dut.h
+++ npc/csrc/dut.h
@@ -40,3 +40,15 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   return true;
 
 }
+const char* regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+void isa_reg_display() {
+  for(int i=0;i<32;i++){
+  printf("%s %08lx\n", regs[i], cpu.gpr[i]);
+  }
+}
