5eba3a562013df6df7f5ce59c6da52726581fa83
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-02 12:47:36 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:47:36 up  1:45,  1 user,  load average: 0.04, 0.05, 0.00

--- npc/csrc/dut.h
+++ npc/csrc/dut.h
@@ -11,6 +11,7 @@ typedef struct {
 } CPU_state;
 
 CPU_state cpu;
+
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   assert(0);
 }
@@ -27,6 +28,20 @@ void difftest_raise_intr(word_t NO) {
   assert(0);
 }
 
+
+const char* regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+void isa_reg_display() {
+  for(int i=0;i<32;i++){
+  printf("%s %08lx\n", regs[i], cpu.gpr[i]);
+  }
+}
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int i;
   for (i = 0; i < 32; i++) {
@@ -39,16 +54,4 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 
   return true;
 
-}
-const char* regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
-
-void isa_reg_display() {
-  for(int i=0;i<32;i++){
-  printf("%s %08lx\n", regs[i], cpu.gpr[i]);
-  }
 }
