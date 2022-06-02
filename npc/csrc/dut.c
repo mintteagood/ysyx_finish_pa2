5218a5660209aa35ea689d8dd444efa2c4b6f8cb
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-02 10:58:45 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:58:45 up 49 min,  1 user,  load average: 0.37, 0.15, 0.05

--- npc/csrc/dut.c
+++ npc/csrc/dut.c
@@ -4,10 +4,10 @@
 #include "../csrc/difftest-def.h"
 #include <dlfcn.h>
 #include <stdio.h>
-void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = 0;
-void (*ref_difftest_regcpy)(void *dut, bool direction) = 0;
-void (*ref_difftest_exec)(uint64_t n) = 0L;
-void (*ref_difftest_raise_intr)(uint64_t NO) = 0;
+void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+void (*ref_difftest_exec)(uint64_t n) = NULL;
+void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 #define CONFIG_DIFFTEST 1
 
 #ifdef CONFIG_DIFFTEST
---
+++ npc/csrc/ref.c
@@ -0,0 +1,26 @@
+#include "../csrc/paddr.h"
+#include "../csrc/utils.h"
+#include "../csrc/difftest-def.h"
+#include <dlfcn.h>
+#include <stdio.h>
+
+void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+  assert(0);
+}
+
+void difftest_regcpy(void *dut, bool direction) {
+  assert(0);
+}
+
+void difftest_exec(uint64_t n) {
+  assert(0);
+}
+
+void difftest_raise_intr(word_t NO) {
+  assert(0);
+}
+
+void difftest_init() {
+  /* Perform ISA dependent initialization. */
+  init_isa();
+}
