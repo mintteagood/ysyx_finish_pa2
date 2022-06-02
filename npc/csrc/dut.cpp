ab1058aaafdc7ae86da9dfcb78ee550a77937fe1
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-02 10:53:56 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:53:56 up 44 min,  1 user,  load average: 0.00, 0.00, 0.01

--- npc/csrc/dut.cpp
+++ npc/csrc/dut.cpp
@@ -4,11 +4,12 @@
 #include "../csrc/difftest-def.h"
 #include <dlfcn.h>
 #include <stdio.h>
-void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void* (ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 #define CONFIG_DIFFTEST 1
+
 #ifdef CONFIG_DIFFTEST
 
 static bool is_skip_ref = false;
