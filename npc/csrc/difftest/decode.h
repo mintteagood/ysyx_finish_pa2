af6ddcfd06c8de3a1d13397316c63bdedbfd4225
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-03 13:42:55 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:42:55 up  1:33,  1 user,  load average: 0.02, 0.05, 0.01

--- npc/csrc/difftest/decode.h
+++ npc/csrc/difftest/decode.h
@@ -2,13 +2,13 @@
 #define __CPU_DECODE_H__
 
 #include "../csrc/isa.h"
-
+#include "../csrc/macro.h"
 #define CONFIG_ITRACE 1
 typedef struct Decode {
   vaddr_t pc;
   vaddr_t snpc; // static next pc
   vaddr_t dnpc; // dynamic next pc
-  ISADecodeInfo isa;
+  DecodeInfo isa;
   IFDEF(CONFIG_ITRACE, char logbuf[128]);
 } Decode;
 
