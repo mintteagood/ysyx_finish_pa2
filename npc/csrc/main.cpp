68fbfc08111ff83ff3116def5e75ec59ce36905e
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-03 13:52:22 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:52:22 up  1:42,  1 user,  load average: 0.05, 0.03, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -6,7 +6,7 @@
 #include "Vysyx_22040175_top.h"
 #include "assert.h"
 #include "../csrc/memory/paddr.h"
-#include "../csrc/difftest/difftest.h"
+//#include "../csrc/difftest/difftest.h"
 
 int port = 1;
 Vysyx_22040175_top *top; 
