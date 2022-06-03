5c98cc256e3828cb3b5b774ca215dac1b4efd84b
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-03 13:15:08 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:15:08 up  1:05,  1 user,  load average: 0.05, 0.03, 0.00

--- npc/csrc/isa.h
+++ npc/csrc/isa.h
@@ -2,7 +2,7 @@
 #define __ISA_H__
 
 // Located at src/isa/$(GUEST_ISA)/include/isa-def.h
-#include <isa-def.h>
+#include "../csrc/isa-def.h"
 
 // The macro `__GUEST_ISA__` is defined in $(CFLAGS).
 // It will be expanded as "x86" or "mips32" ...
