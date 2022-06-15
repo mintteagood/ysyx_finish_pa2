9363c5842925cd09b7f788f61143b9abfe859ca3
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-15 17:45:59 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-48-generic #54~20.04.1-Ubuntu SMP Thu Jun 2 23:37:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:45:59 up  5:35,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -2,9 +2,21 @@
 #include <cpu/cpu.h>
 #include <difftest-def.h>
 #include <memory/paddr.h>
+//自己加
+#include "../nemu/tools/spike-diff/repo/riscv/sim.h"
+static sim_t* s = NULL;
+
+
+
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  //assert(0);
+  //自己加
+  if (direction == DIFFTEST_TO_REF) {
+    s->diff_memcpy(addr, buf, n);
+  } else {
+    assert(0);
+  }
 }
 
 void difftest_regcpy(void *dut, bool direction) {
