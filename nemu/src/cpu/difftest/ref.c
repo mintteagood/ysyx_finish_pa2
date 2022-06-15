9af8dfcbeaf89e5bb85f8139a750755129edc216
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-15 21:00:25 +0800

    >  compile NEMU ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-48-generic #54~20.04.1-Ubuntu SMP Thu Jun 2 23:37:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:00:25 up  8:50,  1 user,  load average: 0.01, 0.02, 0.00

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -3,35 +3,35 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 //自己加
-#include "nemu/tools/spike-diff/repo/riscv/sim.h"
+//#include "nemu/tools/spike-diff/repo/riscv/sim.h"
 
-static sim_t* s = NULL;
+//static sim_t* s = NULL;
 
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  //assert(0);
+  assert(0);
   //自己加
-  if (direction == DIFFTEST_TO_REF) {
+ /* if (direction == DIFFTEST_TO_REF) {
     s->diff_memcpy(addr, buf, n);
   } else {
     assert(0);
-  }
+  }*/
 }
 
 void difftest_regcpy(void *dut, bool direction) {
-  //assert(0);
+  assert(0);
   //自己加
-  if (direction == DIFFTEST_TO_REF) {
+ /* if (direction == DIFFTEST_TO_REF) {
     s->diff_set_regs(dut);
   } else {
     s->diff_get_regs(dut);
-  }
+  }*/
 }
 
 void difftest_exec(uint64_t n) {
-  //assert(0);
+  assert(0);
   //自己加
-  s->diff_step(n);
+ /* s->diff_step(n);*/
 }
 
 void difftest_raise_intr(word_t NO) {
