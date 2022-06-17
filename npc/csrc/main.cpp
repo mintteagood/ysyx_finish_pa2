309953092fc7f14c0e3bfcdf62de43974c8dbedf
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-17 10:11:16 +0800

    >  sim RTL ysyx_22040175 傅腾蛟 Linux melissa-virtual-machine 5.13.0-48-generic #54~20.04.1-Ubuntu SMP Thu Jun 2 23:37:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:11:15 up 29 min,  1 user,  load average: 0.03, 0.05, 0.04

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -61,7 +61,7 @@ static long load_img(char*img_file){
 }
 //加difftest
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
-void (*ref difftest_regcpy)(void *dut, bool direction) = NULL;
+void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(word_t NO) = NULL;
 
